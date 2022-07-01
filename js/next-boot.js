dc9e4e6de53fc5d194ce0ea36e2792c3f005c040
DaGuangyang <1063745644@qq.com>
2022-07-01 09:21:55 +0800

    Site updated: 2022-07-01 09:21:44

--- js/next-boot.js
+++ js/next-boot.js
@@ -53,6 +53,7 @@ NexT.boot.refresh = function() {
   NexT.utils.registerSidebarTOC();
   NexT.utils.registerPostReward();
   NexT.utils.registerVideoIframe();
+  codeUnfold();
 };
 
 NexT.boot.motion = function() {
